potap1986/taskmanager-12

9.1.2 Переносим Api в отдельную директорию
В следующих шагах мы переработаем взаимодейсвие с модулем Api и добавим ему помошников. Подготовим для этого почву, создав директорию api/

Co-authored-by: kicumkicum <kicumkicum@gmail.com>

src/main.js
@@ -6,7 +6,7 @@ import TasksModel from "./model/tasks.js";
 import FilterModel from "./model/filter.js";
 import {render, RenderPosition, remove} from "./utils/render.js";
 import {MenuItem, UpdateType, FilterType} from "./const.js";
-import Api from "./api.js";
+import Api from "./api/index.js";
 
 const AUTHORIZATION = `Basic hS2sd3dfSwcl1sa2j`;
 const END_POINT = `https://12.ecmascript.pages.academy/task-manager`;

neeptoon/intensive-objects-taskmanager

src/main.js
@@ -6,7 +6,7 @@ import TasksModel from "./model/tasks.js";
 import FilterModel from "./model/filter.js";
 import {render, RenderPosition, remove} from "./utils/render.js";
 import {MenuItem, UpdateType, FilterType} from "./const.js";
-import Api from "./api.js";
+import Api from "./api/api.js";
 
 const AUTHORIZATION = `Basic hS2sd3dfSwcl1sa2j`;
 const END_POINT = `https://13.ecmascript.pages.academy/task-manager`;